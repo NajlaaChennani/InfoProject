6dc1a53ded4d389a62a0f2b65afced18d9738d94
aIbtissam <aitlbachiribtissam@gmail.com>
2018-05-26 18:46:45 +0100

    Create
    creation d'une nouvelle vue

--- ProjetScrum/ProjetScrum/Controllers/AcceuilController.cs
+++ ProjetScrum/ProjetScrum/Controllers/AcceuilController.cs
@@ -13,5 +13,9 @@ namespace ProjetScrum.Controllers
         {
             return View();
         }
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
