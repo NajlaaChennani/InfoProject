93248369375c7c97c6612a5b61a6b15874332b86
10Zakaria10 <lachgar.zakaria10@gmail.com>
2018-05-26 17:47:45 +0200

    Ajout du premier controller ( acceuil )

---
+++ ProjetScrum/ProjetScrum/Controllers/AcceuilController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetScrum.Controllers
+{
+    public class AcceuilController : Controller
+    {
+        // GET: Acceuil
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
