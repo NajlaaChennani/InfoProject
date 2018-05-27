176d845464c773d1280fb1dd6c89b0413df0074f
NajlaaChennani <39197870+NajlaaChennani@users.noreply.github.com>
2018-05-27 00:33:11 +0000

    Evenement "Inscription" et "Connexion"

--- ProjetScrum/ProjetScrum/Controllers/LoginController.cs
+++ ProjetScrum/ProjetScrum/Controllers/LoginController.cs
@@ -3,20 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ProjetScrum.Models;
 namespace ProjetScrum.Controllers
 {
     public class LoginController : Controller
     {
+        GLDatabaseEntities entity;
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult LoginTrait(string nom, string prenom, string email, int age, string qst, string mdp)
+        [HttpGet]
+        public ActionResult RegisterTrait(string nom, string prenom, string email, int age, string qst, string mdp)
         {
+            entity = new GLDatabaseEntities();
+            User user = new User();
+            user.Nom = nom;
+            user.Prenom = prenom;
+            user.Email = email;
+            user.Age = age;
+            user.Mdp = mdp;
+            user.Secretquestion = qst;
+            entity.Users.Add(user);
+            entity.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult LoginTrait(string email, string mdp, string session)
+        {
+            entity = new GLDatabaseEntities();
+            Boolean bool1 = false;
+            for(int i=0; i<entity.Users.ToList().Count;i++)
+            {
+                if(entity.Users.ToList().ElementAt(i).Email.Equals(email) && entity.Users.ToList().ElementAt(i).Mdp.Equals(mdp))
+                {
+                    if(session.Equals("true"))
+                    {
+                        Session["user"] = entity.Users.ToList().ElementAt(i);
+                    }
+                    bool1 = true;
+                    break;
+                }
+            }
+            if(bool1)
+            {
+                return Redirect(Url.Action("Index", "Acceuil"));
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
     }
 }
